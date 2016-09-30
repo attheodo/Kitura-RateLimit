3858e97944678ed94cc4fa46daa1b431a5d8ed5f
attheodo <at@atworks.gr>
2016-09-30 17:35:30 +0300

    update package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "kitura-RateLimit",
+    name: "KituraRateLimit",
     dependencies: [
        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
        .Package(url: "https://github.com/IBM-Swift/Kitura-Cache", majorVersion: 1, minor: 0),
