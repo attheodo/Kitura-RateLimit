ae7a93ab8e7bfbbd8db4f97ccee2379ab08fc0e6
attheodo <at@atworks.gr>
2016-10-04 09:56:55 +0300

    fixed invalid format

--- Package.swift
+++ Package.swift
@@ -6,4 +6,3 @@ let package = Package(
        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
        .Package(url: "https://github.com/IBM-Swift/Kitura-Cache", majorVersion: 1, minor: 0),
    ])
-)
